44b89d4c8ca36e429d80147384dba3fb637ef011
Ira <ipraharajgh@gmail.com>
2025-02-03 21:52:49 -0500

    update

--- index.js
+++
@@ -1,4 +0,0 @@
-$('#exampleModal, #exampleModal1').on('show.bs.modal', function (event) {
-    var div = $(event.relatedTarget)
-    $(this).find('.modal-body img').attr("src", div.data('src'));
-  })
